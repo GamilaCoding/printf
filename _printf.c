c761d6195996134b20ecc7c00225d86ad2f6aff7
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-03-25 09:39:09 -0700

    update files

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -53,8 +54,106 @@ void write_format(va_list *args_list, fmt_info_t *fmt_info)
 {
 	int i;
 	spec_printer_t spec_printers[] = {
+		{'%', convert_fmt_percent},
+		{'p', convert_fmt_p},
+		{'c', convert_fmt_c},
+		{'s', convert_fmt_s},
 		{'d', convert_fmt_di},
 		{'i', convert_fmt_di},
-		{'s', convert_fmt_s},
-		{'%', convert_fmt_percent},
-};
+		{'X', convert_fmt_xX},
+		{'x', convert_fmt_xX},
+		{'o', convert_fmt_o},
+		{'u', convert_fmt_u},
+		/* #begin custom specifiers */
+		{'b', convert_fmt_b},
+		{'R', convert_fmt_R},
+		{'r', convert_fmt_r},
+		{'S', convert_fmt_S},
+		/* #end */
+		{'F', convert_fmt_fF},
+		{'f', convert_fmt_fF},
+	};
+
+	for (i = 0; i < 23 && spec_printers[i].spec != '\0'; i++)
+	{
+		if (fmt_info->spec == spec_printers[i].spec)
+		{
+			spec_printers[i].print_arg(args_list, fmt_info);
+			break;
+		}
+	}
+}
+
+/**
+ * _putstr - writes the given string to the buffer
+ * @str: The string to write
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putstr(char *str)
+{
+	int i, out;
+
+	for (i = 0; str && *(str + i) != 0; i++)
+		out = _putchar(*(str + i));
+	return (out);
+}
+
+/**
+ * _putchar - writes the character c to the buffer
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write_to_buffer(c, 0));
+}
+
+/**
+ * write_to_buffer - Writes a char to the buffer based on an action code
+ * @c: The character to write
+ * @action: The action to perform (
+ * -1-> reset the static variables
+ * 0-> write char to buffer
+ * 1-> don't write character to buffer but empty buffer onto stdout
+ * -2-> the number of characters written to stdout)
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int write_to_buffer(char c, char action)
+{
+	static int i;
+	static int chars_count;
+	static char buffer[1024];
+	static char out;
+
+	if (i < 1024 && action == 0)
+	{
+		out = chars_count < 1 ? 1 : out;
+		buffer[i] = c;
+		i++;
+		chars_count++;
+	}
+	if (i >= 1024 || action == 1)
+	{
+		out = write(1, buffer, i);
+		fflush(stdout);
+		i = 0;
+		mem_set(buffer, 1024, 0);
+	}
+	if (action == -1)
+	{
+		i = 0;
+		chars_count = 0;
+		mem_set(buffer, 1024, 0);
+	}
+	if (action == -2)
+	{
+		return (chars_count);
+	}
+	return (out);
+}
